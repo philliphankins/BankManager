8c8dfff61cc97769eca28ba5e5eb1b636e183197
philliphankins <34249287+philliphankins@users.noreply.github.com>
2018-01-16 19:44:55 -0700

    Subclasses added to parent class.

--- src/BankManager.java
+++ src/BankManager.java
@@ -1,4 +1,30 @@
-import com.sun.corba.se.impl.orb.ParserTable;
+import java.util.Date;
 
-public class BankManager {ParserTable.TestAcceptor1
+public class BankManager {
+
+    public static void main(String[] args) {
+
+        class BankAccount {
+            String acctNumber;
+            double balance;
+        }
+
+        class Checking extends BankAccount{
+            double limit;
+        }
+
+        class Savings extends BankAccount{
+            int transfers;
+        }
+
+        class COD extends BankAccount{
+            Date expiry;
+
+
+
+        }
+
+
+    }
+
 }
